6bb24655c6eee83927c4b6f686859da580600dfe
Rehman Ali <rehmanali.9442289@gmail.com>
2020-08-20 08:27:26 +0500

    Add logging using option -logging

--- LaderIndexMap_v2.3.py
+++ LaderIndexMap_v2.3.py
@@ -1,6 +1,12 @@
 import os
 import csv
+import logging
 import argparse
+
+
+logging.basicConfig(
+    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.INFO
+)
 
 parser = argparse.ArgumentParser(description="")
 
@@ -16,40 +22,60 @@ parser.add_argument(
 parser.add_argument("-input", help="Input file", action="store", dest="input")
 parser.add_argument("-output", help="Output file.", action="store", dest="output")
 parser.add_argument("-code", type=int, help="The code.", action="store", dest="code")
+parser.add_argument(
+    "-logging", type=bool, help="Enable Logging.", action="store", dest="log"
+)
 
 
 def process_lader_index_map(input_file, output_file, code):
     calc = f'"{code}*(A>0)"'
-    os.system(
-        f"gdal_calc.py -A gdal_calc.py --NoDataValue=0 --outfile=F_temp.tif -A {input_file} --calc={calc}"
-    )
 
-    os.system(
-        "gdalwarp -tr 300 300 -dstnodata 0 -te -13887126.77 2814162.483 -7430226.771 6360162.483 -t_srs EPSG:3857 -overwrite -r bilinear F_temp.tif  G_temp.tif"
-    )
+    cmd = f"gdal_calc.py -A gdal_calc.py --NoDataValue=0 --outfile=F_temp.tif -A {input_file} --calc={calc}"
+    logging.info(f"Running: {cmd}")
+    os.system(cmd)
 
+    cmd = "gdalwarp -tr 300 300 -dstnodata 0 -te -13887126.77 2814162.483 -7430226.771 6360162.483 -t_srs EPSG:3857 -overwrite -r bilinear F_temp.tif  G_temp.tif"
+    logging.info(f"Running: {cmd}")
+    os.system(cmd)
+
+    if not os.path.exists("F_temp.tif"):
+        logging.info(f"F_temp.tif does not exist!")
     os.system("rm F_temp.tif")
 
-    os.system(
-        'saga_cmd grid_tools "Reclassify Grid Values" -INPUT=G_temp.tif -RESULT=H_temp.sdat -RETAB=matrix.txt -METHOD=0 -OLD=0.0 -NEW=0.0 -SOPERATOR=0 -MIN=0.0 -MAX=1.0 -RNEW=2.0 -ROPERATOR=0 -RETAB -OTHEROPT=false -OTHERS=0.0'
-    )
+    cmd = 'saga_cmd grid_tools "Reclassify Grid Values" -INPUT=G_temp.tif -RESULT=H_temp.sdat -RETAB=matrix.txt -METHOD=0 -OLD=0.0 -NEW=0.0 -SOPERATOR=0 -MIN=0.0 -MAX=1.0 -RNEW=2.0 -ROPERATOR=0 -RETAB -OTHEROPT=false -OTHERS=0.0'
+    logging.info(f"Running: {cmd}")
+    os.system(cmd)
 
     # The command below probably needs testing manually... -TOPERATOR=0 probably.
-    os.system(
-        'saga_cmd io_gdal "Export GeoTIFF" -GRIDS=H_temp.sdat -TOPERATOR=0 -NODATAOPT=true -FILE=J_Temp.tif'
-    )
+    cmd = 'saga_cmd io_gdal "Export GeoTIFF" -GRIDS=H_temp.sdat -TOPERATOR=0 -NODATAOPT=true -FILE=J_Temp.tif'
+    logging.info(f"Running: {cmd}")
+    os.system(cmd)
 
-    os.system("rm H_temp.* G_temp.tif")
+    if not os.path.exists("G_temp.tif"):
+        logging.info(" G_temp.tif does not exist!")
+    if not os.path.exists("H_temp.sdat"):
+        logging.info("H_temp.sdat does not exist@")
+    cmd = "rm H_temp.* G_temp.tif"
+    logging.info(f"Running: {cmd}")
+    os.system(cmd)
 
-    os.system(
-        f"gdal_translate -tr 300 300 -a_nodata 0 -co  COMPRESS=LZW -co JPEG_QUALITY=80 J_Temp.tif {output_file}"
-    )
+    if not os.path.exists("output_file"):
+        logging.info(f"{output_file} file does not exist!")
+    cmd = f"gdal_translate -tr 300 300 -a_nodata 0 -co  COMPRESS=LZW -co JPEG_QUALITY=80 J_Temp.tif {output_file}"
+    logging.info(f"Running: {cmd}")
+    os.system(cmd)
 
-    os.system("rm J_Temp.tif")
+    if not os.path.exists("J_Temp.tif"):
+        logging.info(f"J_Temp.tif file does not exist!")
+    cmd = "rm J_Temp.tif"
+    os.system(cmd)
 
 
 def main():
     args = parser.parse_args()
+    if not args.log:
+        print("Logging is Disabled!")
+        logging.disable(logging.CRITICAL)
     if args.pull_from_file:
         if args.pull_from_file.lower() == "true":
             if not args.input_file:
@@ -62,9 +88,9 @@ def main():
                 print(
                     f"input_file = {row['input_file']}\n output_file = {row['output_file']}\ncode = {row['code']}"
                 )
-                # process_lader_index_map(
-                #     row["input_file"], row["output_file"], row["code"]
-                # )
+                process_lader_index_map(
+                    row["input_file"], row["output_file"], row["code"]
+                )
             print(f"Processed all the entries in {args.input_file}")
     else:
         if not args.input:
