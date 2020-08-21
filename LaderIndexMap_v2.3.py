3b9503af4bc6fabd88ef964028d89c3cf77704a0
Rehman Ali <rehmanali.9442289@gmail.com>
2020-08-21 18:38:34 +0500

    Update all the commands in process_lader_index_map function

--- LaderIndexMap_v2.3.py
+++ LaderIndexMap_v2.3.py
@@ -8,7 +8,7 @@ logging.basicConfig(
     format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.INFO
 )
 
-parser = argparse.ArgumentParser(description="")
+parser = argparse.ArgumentParser(description="LaderIndexMap Process")
 
 parser.add_argument(
     "-pull_from_file",
@@ -20,8 +20,10 @@ parser.add_argument(
     "-input_file", help="Input file in csv format.", action="store", dest="input_file"
 )
 parser.add_argument("-input", help="Input file", action="store", dest="input")
-parser.add_argument("-output", help="Output file.", action="store", dest="output")
-parser.add_argument("-code", type=int, help="The code.", action="store", dest="code")
+parser.add_argument("-output", help="Output file.",
+                    action="store", dest="output")
+parser.add_argument("-code", type=int, help="The code.",
+                    action="store", dest="code")
 parser.add_argument(
     "-logging", type=bool, help="Enable Logging.", action="store", dest="log"
 )
@@ -30,11 +32,11 @@ parser.add_argument(
 def process_lader_index_map(input_file, output_file, code):
     calc = f'"{code}*(A>0)"'
 
-    cmd = f"gdal_calc.py -A gdal_calc.py --NoDataValue=0 --outfile=F_temp.tif -A {input_file} --calc={calc}"
+    cmd = f"gdal_calc.py gdal_calc.py -A gdal_calc.py --NoDataValue=-99999 --outfile=F_temp.tif -A {input_file} --calc={calc}"
     logging.info(f"Running: {cmd}")
     os.system(cmd)
 
-    cmd = "gdalwarp -tr 300 300 -dstnodata 0 -te -13887126.77 2814162.483 -7430226.771 6360162.483 -t_srs EPSG:3857 -overwrite -r bilinear F_temp.tif  G_temp.tif"
+    cmd = "gdalwarp -tr 300 300 -dstnodata 0 -te -10757853.456188 4920683.977243 -10034349.767885 5388556.189920 -t_srs EPSG:3857 -overwrite -r bilinear F_temp.tif G_temp.tif"
     logging.info(f"Running: {cmd}")
     os.system(cmd)
 
@@ -42,12 +44,13 @@ def process_lader_index_map(input_file, output_file, code):
         logging.info(f"F_temp.tif does not exist!")
     os.system("rm F_temp.tif")
 
-    cmd = 'saga_cmd grid_tools "Reclassify Grid Values" -INPUT=G_temp.tif -RESULT=H_temp.sdat -RETAB=matrix.txt -METHOD=0 -OLD=0.0 -NEW=0.0 -SOPERATOR=0 -MIN=0.0 -MAX=1.0 -RNEW=2.0 -ROPERATOR=0 -RETAB -OTHEROPT=false -OTHERS=0.0'
+    cmd = 'saga_cmd grid_tools "Reclassify Grid Values" -INPUT=G_temp.tif -RESULT=H_temp.sdat -RETAB=matrix.txt -METHOD=0 -OLD=0.0 -NEW=0.0 -SOPERATOR=0 -MIN=0.0 -MAX=1.0 -RNEW=2.0 -ROPERATOR=0 -RETAB -OTHEROPT=0.0 -OTHERS=0.0'
     logging.info(f"Running: {cmd}")
     os.system(cmd)
 
     # The command below probably needs testing manually... -TOPERATOR=0 probably.
-    cmd = 'saga_cmd io_gdal "Export GeoTIFF" -GRIDS=H_temp.sdat -TOPERATOR=0 -NODATAOPT=true -FILE=J_Temp.tif'
+
+    cmd = 'saga_cmd io_gdal "Export GeoTIFF" -GRIDS=H_temp.sdat -FORMAT=1 -FILE=J_Temp.tif -TYPE=5 -NODATA'
     logging.info(f"Running: {cmd}")
     os.system(cmd)
 
@@ -55,13 +58,15 @@ def process_lader_index_map(input_file, output_file, code):
         logging.info(" G_temp.tif does not exist!")
     if not os.path.exists("H_temp.sdat"):
         logging.info("H_temp.sdat does not exist@")
-    cmd = "rm H_temp.* G_temp.tif"
+
+    cmd = 'rm H_temp.* G_temp.tif'
     logging.info(f"Running: {cmd}")
     os.system(cmd)
 
     if not os.path.exists("output_file"):
         logging.info(f"{output_file} file does not exist!")
-    cmd = f"gdal_translate -tr 300 300 -a_nodata 0 -co  COMPRESS=LZW -co JPEG_QUALITY=80 J_Temp.tif {output_file}"
+
+    cmd = f'gdal_translate -tr 300 300 -a_nodata -99999 -co COMPRESS=LZW -co JPEG_QUALITY=90 J_Temp.tif {output_file}'
     logging.info(f"Running: {cmd}")
     os.system(cmd)
 
@@ -79,7 +84,8 @@ def main():
     if args.pull_from_file:
         if args.pull_from_file.lower() == "true":
             if not args.input_file:
-                parser.error("Input file is required when pull_from_file is true.")
+                parser.error(
+                    "Input file is required when pull_from_file is true.")
             if not os.path.exists(args.input_file):
                 parser.error("Input does not exists!")
             csv_file = open(args.input_file, "r")
@@ -91,7 +97,7 @@ def main():
                 process_lader_index_map(
                     row["input_file"], row["output_file"], row["code"]
                 )
-            print(f"Processed all the entries in {args.input_file}")
+            logging.info(f"Processed all the entries in {args.input_file}")
     else:
         if not args.input:
             parser.error("Provide an input file using -input arg ...")
