01fd45c9e6fcebea6ac4b83e247561740020d267
Rehman Ali <rehmanali.9442289@gmail.com>
2020-08-21 22:01:21 +0500

    Update commands again

--- LaderIndexMap_v2.3.py
+++ LaderIndexMap_v2.3.py
@@ -32,27 +32,27 @@ parser.add_argument(
 def process_lader_index_map(input_file, output_file, code):
     calc = f'"{code}*(A>0)"'
 
-    cmd = f"gdal_calc.py gdal_calc.py -A gdal_calc.py --NoDataValue=-99999 --outfile=F_temp.tif -A {input_file} --calc={calc}"
-    logging.info(f"Running: {cmd}")
-    os.system(cmd)
+    cmd1 = f'gdal_calc.py gdal_calc.py -A gdal_calc.py --outfile=F_temp.tif -A {input_file} --calc={calc}'
+    logging.info(f"Running: {cmd1}")
+    os.system(cmd1)
 
-    cmd = "gdalwarp -tr 300 300 -dstnodata 0 -te -10757853.456188 4920683.977243 -10034349.767885 5388556.189920 -t_srs EPSG:3857 -overwrite -r bilinear F_temp.tif G_temp.tif"
-    logging.info(f"Running: {cmd}")
-    os.system(cmd)
+    cmd2 = 'gdalwarp -tr 200 200 -dstnodata 0 -t_srs EPSG:3857 -overwrite -r bilinear F_temp.tif G_temp.tif'
+    logging.info(f"Running: {cmd2}")
+    os.system(cmd2)
 
     if not os.path.exists("F_temp.tif"):
         logging.info(f"F_temp.tif does not exist!")
     # os.system("rm F_temp.tif")
 
-    cmd = 'saga_cmd grid_tools "Reclassify Grid Values" -INPUT=G_temp.tif -RESULT=H_temp.sdat -RETAB=matrix.txt -METHOD=0 -OLD=0.0 -NEW=0.0 -SOPERATOR=0 -MIN=0.0 -MAX=1.0 -RNEW=2.0 -ROPERATOR=0 -RETAB -OTHEROPT=0.0 -OTHERS=0.0'
-    logging.info(f"Running: {cmd}")
-    os.system(cmd)
+    cmd3 = 'saga_cmd grid_tools "Reclassify Grid Values" -INPUT=G_temp.tif -RESULT=H_temp.sdat -RETAB=matrix.txt -METHOD=0 -OLD=0.0 -NEW=0.0 -SOPERATOR=0 -MIN=0.0 -MAX=1.0 -RNEW=2.0 -ROPERATOR=0 -RETAB -OTHEROPT=0.0 -OTHERS=0.0'
+    logging.info(f"Running: {cmd3}")
+    os.system(cmd3)
 
     # The command below probably needs testing manually... -TOPERATOR=0 probably.
 
-    cmd = 'saga_cmd io_gdal "Export GeoTIFF" -GRIDS=H_temp.sdat -FORMAT=1 -FILE=J_Temp.tif -TYPE=5 -NODATA'
-    logging.info(f"Running: {cmd}")
-    os.system(cmd)
+    cmd4 = 'saga_cmd io_gdal "Export GeoTIFF" -GRIDS=H_temp.sdat -FILE=J_Temp.tifs'
+    logging.info(f"Running: {cmd4}")
+    os.system(cmd4)
 
     if not os.path.exists("G_temp.tif"):
         logging.info(" G_temp.tif does not exist!")
@@ -66,9 +66,9 @@ def process_lader_index_map(input_file, output_file, code):
     if not os.path.exists("output_file"):
         logging.info(f"{output_file} file does not exist!")
 
-    cmd = f'gdal_translate -tr 300 300 -a_nodata -99999 -co COMPRESS=LZW -co JPEG_QUALITY=90 J_Temp.tif {output_file}'
-    logging.info(f"Running: {cmd}")
-    os.system(cmd)
+    cmd5 = f'gdal_translate -tr 200 200 -co COMPRESS=LZW -a_nodata 0 -co JPEG_QUALITY=90 J_Temp.tif {output_file}'
+    logging.info(f"Running: {cmd5}")
+    os.system(cmd5)
 
     if not os.path.exists("J_Temp.tif"):
         logging.info(f"J_Temp.tif file does not exist!")
